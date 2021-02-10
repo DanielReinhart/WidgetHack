38a75a78e1d96702819193aae66e04a1f8a74c76
Daniel Reinhart <danielr@procore.com>
2021-02-10 16:09:25 -0600

    Initial Commit

---
+++ WidgetHack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WidgetHack
+//
+//  Created by Daniel Reinhart on 2/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WidgetHack/WidgetHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetHackApp.swift
+//  WidgetHack
+//
+//  Created by Daniel Reinhart on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WidgetHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
